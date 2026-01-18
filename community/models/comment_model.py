4daf83a87b3fdb34971b0777f559d620f7add9ee
junyoung0502 <jylee0005@gmail.com>
2026-01-18 22:22:13 +0900

    docs: convert comments to docstrings

--- community/models/comment_model.py
+++ community/models/comment_model.py
@@ -6,9 +6,9 @@ comments_db = []
 
 class CommentModel:
     
-    # 댓글 생성 메서드
     @staticmethod
     def create_comment(comment_data: dict):
+        '''새 댓글 생성 및 저장'''
         new_id = 1
         if comments_db:
             new_id = max(comment["commentId"] for comment in comments_db) + 1
@@ -17,31 +17,31 @@ class CommentModel:
         comments_db.append(comment_data)
         return new_id
 
-    # 댓글 목록 조회 메서드
     @staticmethod
     def get_comments_by_post_id(post_id: int):
+        '''특정 게시글의 댓글 목록 조회'''
         # 리스트 컴프리헨션으로 필터링 (SQL의 WHERE post_id = ? 와 같음)
         return [comment for comment in comments_db if comment["postId"] == post_id]
 
-    # 댓글 하나 조회 메서드
     @staticmethod
     def get_comment_by_id(comment_id: int):
+        '''특정 댓글 조회'''
         for comment in comments_db:
             if comment["commentId"] == comment_id:
                 return comment
         return None
 
-    # 댓글 삭제 메서드
     @staticmethod
     def delete_comment(comment_id: int):
+        '''특정 댓글 삭제'''
         for comment in comments_db:
             if comment["commentId"] == comment_id:
                 comments_db.remove(comment)
                 return True
         return False
     
-    # 게시글이 삭제될 때 해당 게시글의 모든 댓글도 삭제
     @staticmethod
     def delete_comments_by_post_id(post_id: int):
+        '''특정 게시글에 달린 모든 댓글 삭제'''
         # postId가 지우려는 post_id와 다른 댓글만 남기고 삭제
         comments_db[:] = [c for c in comments_db if c["postId"] != post_id]
